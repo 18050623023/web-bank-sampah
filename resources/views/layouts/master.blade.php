8fe64b7329c0475237f061116c8e4399538495af
yazidaby1980 <47811965+yazidaby1980@users.noreply.github.com>
2023-12-19 00:35:54 +0700

    register role petugas

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--favicon-->
-    <link rel="icon" href="{{ asset('template') }}/assets/images/dlh.png" type="image/png" />
+    <link rel="icon" href="{{ asset('template') }}/assets/images/earntrash.png" type="image/png" />
     <!--plugins-->
     <link href="{{ asset('template') }}/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
     <link href="{{ asset('template') }}/assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
