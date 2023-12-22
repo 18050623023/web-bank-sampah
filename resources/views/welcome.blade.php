7fc456a354ea1543e7faa350be343c7f68d3ec2b
18050623023 <47519476+18050623023@users.noreply.github.com>
2023-12-22 15:13:49 +0700

    update Javascript

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,12 +78,12 @@
     </div>
     <!--end wrapper-->
     <!-- Bootstrap JS -->
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('template') }}/assets/js/bootstrap.bundle.min.js"></script>
     <!--plugins-->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
-    <script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
-    <script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+    <script src="{{ asset('template') }}/assets/js/jquery.min.js"></script>
+    <script src="{{ asset('template') }}/assets/plugins/simplebar/js/simplebar.min.js"></script>
+    <script src="{{ asset('template') }}/assets/plugins/metismenu/js/metisMenu.min.js"></script>
+    <script src="{{ asset('template') }}/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
     <!--Password show & hide js -->
     <script>
         $(document).ready(function() {
@@ -102,7 +102,7 @@
         });
     </script>
     <!--app JS-->
-    <script src="assets/js/app.js"></script>
+    <script src="{{ asset('template') }}/assets/js/app.js"></script>
 </body>
 
 </html>
